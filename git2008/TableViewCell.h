9640c306eb0ff75515b9f34008c0a73c41809d84
Lizihao Li <lizihaoli@Lizihaos-MacBook-Air.local>
2020-05-17 16:08:12 +0800

    文件增加

---
+++ git2008/TableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.h
+//  git2008
+//
+//  Created by Lizihao Li on 2020/5/17.
+//  Copyright © 2020 Lizihao Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ git2008/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  git2008
+//
+//  Created by Lizihao Li on 2020/5/17.
+//  Copyright © 2020 Lizihao Li. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
