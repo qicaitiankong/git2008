0c75c68f9a5ce1b9f0969cba01cd1c714a2bf3cb
Lizihao Li <lizihaoli@Lizihaos-MacBook-Air.local>
2020-05-17 15:45:27 +0800

    测试001

--- git2008/AppDelegate.m
+++ git2008/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //注释
+    UIViewController *vc = [[UIViewController alloc]init];
     return YES;
 }
 
